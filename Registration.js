b4a32092751aa7757c6289e6a88cb76dceaa7f5d
Joey Flynn <josephpflynn25@gmail.com>
2023-10-10 14:28:40 -0400

    testCommit

---
+++ Registration.js
@@ -0,0 +1,8 @@
+function submit(){
+
+    var email = document.getElementById("email").value;
+
+    var pwd = document.getElementById("pwd").value;
+
+    
+}
---
+++ RegistrationFront.php
@@ -0,0 +1,19 @@
+
+
+<html>
+    <header>
+        <scirpt src="Registration.js"></scirpt>
+    </header>
+
+    <body>
+        <form method="post" name="user" style="font-family:arial; font-size:25px">
+            Email: <br>
+            <input type="email" id="email"> <br>
+            Password: <br>
+            <input type="password" id="pwd"> <br>
+
+            <input type="submit" value="Submit" onclick="submit()">         
+            
+        </form>
+    </body>
+</html>
